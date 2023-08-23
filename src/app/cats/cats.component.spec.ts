093cb9dcfc9b3f3b6db95221bafdc49cc17972d5
Ciril Saner <Ciril.Saner@bit.admin.ch>
2023-08-23 14:49:54 +0200

    fix unit test console errors

--- src/app/cats/cats.component.spec.ts
+++ src/app/cats/cats.component.spec.ts
@@ -1,6 +1,8 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {CatsComponent} from './cats.component';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {MatTableModule} from '@angular/material/table';
+import {TranslateModule} from '@ngx-translate/core';
 
 describe('CatsComponent', () => {
 	let component: CatsComponent;
@@ -8,7 +10,7 @@ describe('CatsComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [HttpClientTestingModule],
+			imports: [HttpClientTestingModule, MatTableModule, TranslateModule.forRoot()],
 			declarations: [CatsComponent]
 		}).compileComponents();
 
